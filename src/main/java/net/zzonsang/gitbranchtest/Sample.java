80c748953cd525a4d65385317b9ecdf6b009277d
zzonsang <zzonsang2@gmail.com>
2012-08-30 16:23:23 +0900

    feature/myfeature 신규 생성 후 Sample  파일 내용 수정. Sample3 클래스 생성

--- src/main/java/net/zzonsang/gitbranchtest/Sample.java
+++ src/main/java/net/zzonsang/gitbranchtest/Sample.java
@@ -3,5 +3,6 @@ package net.zzonsang.gitbranchtest;
 public class Sample {
 	public void test() {
 		System.out.println("New feature!!");
+		System.out.println("Add feature/myfeature");
 	}
 }
---
+++ src/main/java/net/zzonsang/gitbranchtest/Sample3.java
@@ -0,0 +1,8 @@
+package net.zzonsang.gitbranchtest;
+
+public class Sample3 {
+
+	public void test() {
+		System.out.println("[New] feature/myfeature");
+	}
+}
